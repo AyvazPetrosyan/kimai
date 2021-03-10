5c42a8f1e8f6e00008e6ff6433027e61a1c12d4e
AyvazPetrosyan <ayvazpetrosyan@gmail.com>
2021-03-10 15:13:54 +0400

    added duretion update function

---
+++ src/Timesheet/Calculator/DurationCalculator.php
@@ -0,0 +1,48 @@
+<?php
+
+/*
+ * This file is part of the Kimai time-tracking app.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\Timesheet\Calculator;
+
+use App\Entity\Timesheet;
+use App\Timesheet\CalculatorInterface;
+use App\Timesheet\RoundingService;
+
+/**
+ * Implementation to calculate the durations for a timesheet record.
+ */
+final class DurationCalculator implements CalculatorInterface
+{
+    /**
+     * @var RoundingService
+     */
+    private $roundings;
+
+    public function __construct(RoundingService $roundings)
+    {
+        $this->roundings = $roundings;
+    }
+
+    /**
+     * @param Timesheet $record
+     */
+    public function calculate(Timesheet $record)
+    {
+        if (null === $record->getEnd()) {
+            return;
+        }
+
+        $duration = $record->getEnd()->getTimestamp() - $record->getBegin()->getTimestamp();
+//        $mDuration = $record->getEnd()->getTimestamp() - $record->getBegin()->getTimestamp();
+//        $aDuration = $record->getMEnd()->getTimestamp() - $record->getMBegin()->getTimestamp();
+//        $duration = $mDuration + $aDuration;
+        $record->setDuration($duration);
+
+        $this->roundings->applyRoundings($record);
+    }
+}
